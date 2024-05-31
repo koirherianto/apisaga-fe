db11d8204610263846265f39a2ce546e2cb960da
koirherianto <koir.harianto@gmail.com>
2024-05-31 21:17:09 +0800

    .

---
+++ src/routes/+page.svelte
@@ -0,0 +1,48 @@
+<div class="container">
+    <div class="row m-5">
+        <div class="col-md-10">
+            <h1>Your Project</h1>
+        </div>
+        <div class="col-md-2">
+            <!-- tombol logout & login-->
+            <a href="/login" class="btn btn-primary">Logout</a>  
+            <a href="/profil" class="btn btn-primary">Profil</a>  
+        </div>
+    </div>
+    <div class="row m-5">
+		<div class="col-md-4">
+			<div class="card mb-3" style="width: 18rem;">
+				<div class="card-body">
+					<h5 class="card-title">Special title treatment</h5>
+					<p class="card-text">
+						With supporting text below as a natural lead-in to additional content.
+					</p>
+					<a href="#" class="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-4">
+			<div class="card text-center mb-3" style="width: 18rem;">
+				<div class="card-body">
+					<h5 class="card-title">Special title treatment</h5>
+					<p class="card-text">
+						With supporting text below as a natural lead-in to additional content.
+					</p>
+					<a href="#" class="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="card text-end" style="width: 18rem;">
+				<div class="card-body">
+					<h5 class="card-title">Special title treatment</h5>
+					<p class="card-text">
+						With supporting text below as a natural lead-in to additional content.
+					</p>
+					<a href="#" class="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,37 @@
+<div class="container m-5 p-5">
+	<form method="post" action="">
+		<div class="row mb-4">
+			<div class="col">
+				<div data-mdb-input-init class="form-outline">
+					<input type="text" name="name" id="name" class="form-control" />
+					<label class="form-label" for="name">Name</label>
+				</div>
+			</div>
+			<div class="col">
+				<div data-mdb-input-init class="form-outline">
+					<input type="text" name="username" id="username" class="form-control" />
+					<label class="form-label" for="username">Username</label>
+				</div>
+			</div>
+		</div>
+
+		<div class="row mb-4">
+			<div class="col">
+				<!-- Email input -->
+				<div data-mdb-input-init class="form-outline mb-4">
+					<input type="email" name="email" id="email" class="form-control" />
+					<label class="form-label" for="email">Email address</label>
+				</div>
+			</div>
+			<div class="col">
+				<div data-mdb-input-init class="form-outline mb-4">
+					<input type="password" name="password" id="passoword" class="form-control" />
+					<label class="form-label" for="passoword">Password</label>
+				</div>
+			</div>
+		</div>
+
+		<!-- Submit button -->
+		<button type="submit" class="btn btn-primary btn-block mb-4">Sign up</button>
+	</form>
+</div>
