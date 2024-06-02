3ef6a00735fe71f9620e72346785348ccce51dc5
koirherianto <koir.harianto@gmail.com>
2024-06-02 20:17:34 +0800

    register ssr

---
+++ src/routes/register/+page.server.ts
@@ -0,0 +1,66 @@
+import { redirect } from '@sveltejs/kit';
+import { fail } from '@sveltejs/kit';
+import type { PageServerLoad, Actions } from './$types';
+
+export const load: PageServerLoad = async ({cookies}) => {
+	const token = cookies.get('token');
+    const userString = cookies.get('user');
+    const user = userString ? JSON.parse(userString) : null;
+    
+    if (token && user) {
+        redirect(307, '/u/' + user!.username +'/project');
+    }
+};
+
+export const actions = {
+    register: async ({cookies, request})  => {
+        const form = await request.formData();
+
+        const body = {
+            name : form.get('name'),
+            email : form.get('email'),
+            username : form.get('username'),
+            password : form.get('password'),
+        }
+
+        const response = await fetch('http://localhost:3333/api/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body),
+        });
+
+        const responseData = await response.json();
+
+        if (response.status === 201) {
+            cookies.set('token', responseData.token.token, { path: '/' });
+            cookies.set('user', JSON.stringify(responseData.data), { path: '/' });
+            redirect(307, '/u/' + responseData.data.username +'/project');
+        }else if (response.status === 401) {
+            return fail(401, {
+				description: 'a',
+				error: {
+                    'email' : responseData.message,
+                    'password' : responseData.message
+                },
+                old: {
+                    'email' : body.email,
+                    'password' : ''
+                }
+			});
+        } else {
+            return fail(500, {
+                description: 'a',
+                error: {
+                    'email' : responseData.message,
+                    'password' : responseData.message
+                },
+                old : {
+                    'email' : body.email,
+                    'password' : ''
+                }
+            });
+        }
+    }
+} satisfies Actions;
