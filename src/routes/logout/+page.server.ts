e876553c9edb0a305aa7b7f31c41179d800e5a82
koirherianto <koir.harianto@gmail.com>
2024-06-03 16:32:27 +0800

    LOGOUT

--- src/routes/logout/+page.server.ts
+++ src/routes/logout/+page.server.ts
@@ -2,11 +2,40 @@ import type { PageServerLoad } from "../u/$types";
 import { redirect } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({cookies}) => {
-    
-    // delete all cookies
-    cookies.delete('token', { path: '/' });
-    cookies.delete('user', { path: '/' });
+    const token = cookies.get('token');
+    const userString = cookies.get('user');
+    const user = userString ? JSON.parse(userString) : null;
 
-    // redirect to login
-    redirect(307, '/login');
+    if (!token && !user) {
+        return {
+            status: 401,
+            redirect: '/login',
+        };
+    }
+
+    const response = await fetch('http://localhost:3333/api/logout', {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+    });
+
+    const responseData = await response.json();
+
+    console.log(responseData);
+    
+
+    if (response.status === 200) {
+        // delete all cookies
+        cookies.delete('token', { path: '/' });
+        cookies.delete('user', { path: '/' });
+
+        // redirect to login
+        redirect(307, '/login');
+    } else {
+        redirect(307, '/u/' + user.username +'/project')
+    }
+    
+    
 };
