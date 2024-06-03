d1e5adf6e73a549467410f99a47673d53b7f0f8e
koirherianto <koir.harianto@gmail.com>
2024-06-03 16:39:34 +0800

    login page update

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -11,7 +11,7 @@
 		<img
 			class="mx-auto h-10 w-auto"
 			src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-			alt="Your Company"
+			alt="Logo Company"
 		/>
 		<h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
 			Sign in to your account
@@ -79,9 +79,9 @@
 
 		<p class="mt-10 text-center text-sm text-gray-500">
 			Not a member?
-			<a href="/" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-				>Start a 14 day free trial</a
-			>
+			<a href="/register" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+				>Register this shit
+			</a>
 		</p>
 	</div>
 </div>
