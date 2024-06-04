7cce738a4e2f61b3171a2953dca51b757b2ca8a2
koirherianto <koir.harianto@gmail.com>
2024-06-04 16:45:35 +0800

    ui profile

--- src/routes/u/[username]/profile/+page.svelte
+++ src/routes/u/[username]/profile/+page.svelte
@@ -1,16 +1,21 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	export let data;
+</script>
+
 <section class="bg-white dark:bg-gray-900">
 	<div class="container px-6 py-10 mx-auto">
 		<h1 class="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">
-			explore our <br /> awesome <span class="underline decoration-blue-500">Components</span>
+			Your <br /> awesome <span class="underline decoration-blue-500">Profile</span>
 		</h1>
 
 		<p class="mt-4 text-gray-500 xl:mt-6 dark:text-gray-300">
-			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum quam voluptatibus
+			This information will be displayed publicly so be careful what you share.
 		</p>
 		<form>
 			<div class="space-y-12">
 				<div class="border-b border-gray-900/10 pb-12">
-					<h2 class="text-base font-semibold leading-7 text-gray-900">Profile</h2>
+					<!-- <h2 class="text-base font-semibold leading-7 text-gray-900">Profile</h2>
 					<p class="mt-1 text-sm leading-6 text-gray-600">
 						This information will be displayed publicly so be careful what you share.
 					</p>
@@ -115,7 +120,7 @@
 								</div>
 							</div>
 						</div>
-					</div>
+					</div> -->
 				</div>
 
 				<div class="border-b border-gray-900/10 pb-12">
@@ -125,7 +130,7 @@
 					</p>
 
 					<div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-						<div class="sm:col-span-3">
+						<!-- <div class="sm:col-span-3">
 							<label for="first-name" class="block text-sm font-medium leading-6 text-gray-900"
 								>First name</label
 							>
@@ -153,8 +158,23 @@
 									class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
 								/>
 							</div>
-						</div>
+						</div> -->
 
+						<div class="sm:col-span-4">
+							<label for="email" class="block text-sm font-medium leading-6 text-gray-900"
+								>Name</label
+							>
+							<div class="mt-2">
+								<input
+									id="name"
+									name="name"
+									type="text"
+									value="{data.user.name}"
+									autocomplete="given-name"
+									class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+								/>
+							</div>
+						</div>
 						<div class="sm:col-span-4">
 							<label for="email" class="block text-sm font-medium leading-6 text-gray-900"
 								>Email address</label
@@ -165,12 +185,36 @@
 									name="email"
 									type="email"
 									autocomplete="email"
+									value="{data.user.email}"
 									class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
 								/>
 							</div>
 						</div>
+						<div class="sm:col-span-4">
+							<label for="username" class="block text-sm font-medium leading-6 text-gray-900"
+								>Username</label
+							>
+							<div class="mt-1">
+								<div
+									class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+								>
+									<span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm"
+										>apisaga.com/u/</span
+									>
+									<input
+										type="text"
+										name="username"
+										id="username"
+										autocomplete="username"
+										value="{data.user.username}"
+										placeholder="username"
+										class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+									/>
+								</div>
+							</div>
+						</div>
 
-						<div class="sm:col-span-3">
+						<!-- <div class="sm:col-span-3">
 							<label for="country" class="block text-sm font-medium leading-6 text-gray-900"
 								>Country</label
 							>
@@ -186,9 +230,9 @@
 									<option>Mexico</option>
 								</select>
 							</div>
-						</div>
+						</div> -->
 
-						<div class="col-span-full">
+						<!-- <div class="col-span-full">
 							<label for="street-address" class="block text-sm font-medium leading-6 text-gray-900"
 								>Street address</label
 							>
@@ -201,9 +245,9 @@
 									class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
 								/>
 							</div>
-						</div>
+						</div> -->
 
-						<div class="sm:col-span-2 sm:col-start-1">
+						<!-- <div class="sm:col-span-2 sm:col-start-1">
 							<label for="city" class="block text-sm font-medium leading-6 text-gray-900"
 								>City</label
 							>
@@ -246,11 +290,12 @@
 									class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
 								/>
 							</div>
-						</div>
+						</div> -->
 					</div>
 				</div>
 
-				<div class="border-b border-gray-900/10 pb-12">
+				<!-- push notification -->
+				<!-- <div class="border-b border-gray-900/10 pb-12">
 					<h2 class="text-base font-semibold leading-7 text-gray-900">Notifications</h2>
 					<p class="mt-1 text-sm leading-6 text-gray-600">
 						We'll always let you know about important changes, but you pick what else you want to
@@ -309,6 +354,7 @@
 								</div>
 							</div>
 						</fieldset>
+
 						<fieldset>
 							<legend class="text-sm font-semibold leading-6 text-gray-900"
 								>Push Notifications</legend
@@ -356,7 +402,7 @@
 							</div>
 						</fieldset>
 					</div>
-				</div>
+				</div> -->
 			</div>
 
 			<div class="mt-6 flex items-center justify-end gap-x-6">
